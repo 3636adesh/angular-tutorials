02d5e768ad86f49aa00b8b8b2e2bfeffd21cb0a1
Adesh Malunjkar <adesh.malunjkar@smartdocs.ai>
2024-01-04 16:34:04 +0530

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SellerComponent } from './seller/seller.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
-      HeaderComponent
+      HeaderComponent,
+      HomeComponent,
+      SellerComponent,
+      SellerAuthComponent
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
